d03131d63efa0ffe3559f64680a3c567bfc0d4c4
zaynzzz <89463457+zaynzzz@users.noreply.github.com>
2024-09-27 17:36:49 +0700

    Update response.php

--- response.php
+++ response.php
@@ -15,189 +15,55 @@ $action = isset($_POST['action']) ? $_POST['action'] : "";
 
 if ($action == 'email_invoice') {
 
-    $emailId = $_POST['email']; // Ambil email penerima
-    $invoice_data = $_POST['invoice_data']; // Data invoice yang akan ditampilkan (misalnya total, tanggal, dll.)
+    $fileId = $_POST['id'];
+    $emailId = $_POST['email'];
+    $invoice_type = $_POST['invoice_type'];
+    $custom_email = $_POST['custom_email'];
 
     require_once('class.phpmailer.php');
 
-    $mail = new PHPMailer();
+    $mail = new PHPMailer(); // defaults to using php "mail()"
+
+    // Set sender and recipient info
     $mail->AddReplyTo(EMAIL_FROM, EMAIL_NAME);
     $mail->SetFrom(EMAIL_FROM, EMAIL_NAME);
     $mail->AddAddress($emailId, "");
-    $mail->Subject = "Your Invoice";
 
-    // Susun email dengan HTML untuk menyerupai PDF
-    $htmlContent = '
-        <html>
-        <head>
-            <style>
-                body { font-family: Arial, sans-serif; }
-                .invoice-box {
-                    max-width: 800px;
-                    margin: auto;
-                    padding: 30px;
-                    border: 1px solid #eee;
-                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-                    font-size: 16px;
-                    line-height: 24px;
-                    color: #555;
-                }
-                .invoice-box table {
-                    width: 100%;
-                    line-height: inherit;
-                    text-align: left;
-                    border-collapse: collapse;
-                }
-                .invoice-box table td {
-                    padding: 5px;
-                    vertical-align: top;
-                }
-                .invoice-box table tr td:nth-child(2) {
-                    text-align: right;
-                }
-                .invoice-box table tr.top table td {
-                    padding-bottom: 20px;
-                }
-                .invoice-box table tr.information table td {
-                    padding-bottom: 40px;
-                }
-                .invoice-box table tr.heading td {
-                    background: #eee;
-                    border-bottom: 1px solid #ddd;
-                    font-weight: bold;
-                }
-                .invoice-box table tr.details td {
-                    padding-bottom: 20px;
-                }
-                .invoice-box table tr.item td{
-                    border-bottom: 1px solid #eee;
-                }
-                .invoice-box table tr.item.last td {
-                    border-bottom: none;
-                }
-                .invoice-box table tr.total td:nth-child(2) {
-                    border-top: 2px solid #eee;
-                    font-weight: bold;
-                }
-            </style>
-        </head>
-        <body>
-            <div class="invoice-box">
-                <table cellpadding="0" cellspacing="0">
-                    <tr class="top">
-                        <td colspan="2">
-                            <table>
-                                <tr>
-                                    <td class="title">
-                                        <img src="https://yourwebsite.com/logo.png" style="width:100%; max-width:300px;">
-                                    </td>
-                                    
-                                    <td>
-                                        Invoice #: 123<br>
-                                        Created: ' . date('M d, Y') . '<br>
-                                        Due: ' . date('M d, Y', strtotime('+30 days')) . '
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    
-                    <tr class="information">
-                        <td colspan="2">
-                            <table>
-                                <tr>
-                                    <td>
-                                        Your Company, Inc.<br>
-                                        12345 Street Name<br>
-                                        City, State 12345
-                                    </td>
-                                    
-                                    <td>
-                                        ' . $emailId . '<br>
-                                        John Doe<br>
-                                        john@example.com
-                                    </td>
-                                </tr>
-                            </table>
-                        </td>
-                    </tr>
-                    
-                    <tr class="heading">
-                        <td>
-                            Payment Method
-                        </td>
-                        
-                        <td>
-                            Check #
-                        </td>
-                    </tr>
-                    
-                    <tr class="details">
-                        <td>
-                            Check
-                        </td>
-                        
-                        <td>
-                            1000
-                        </td>
-                    </tr>
-                    
-                    <tr class="heading">
-                        <td>
-                            Item
-                        </td>
-                        
-                        <td>
-                            Price
-                        </td>
-                    </tr>
-                    
-                    <tr class="item">
-                        <td>
-                            Website design
-                        </td>
-                        
-                        <td>
-                            $300.00
-                        </td>
-                    </tr>
-                    
-                    <tr class="item">
-                        <td>
-                            Hosting (3 months)
-                        </td>
-                        
-                        <td>
-                            $75.00
-                        </td>
-                    </tr>
-                    
-                    <tr class="item last">
-                        <td>
-                            Domain name (1 year)
-                        </td>
-                        
-                        <td>
-                            $10.00
-                        </td>
-                    </tr>
-                    
-                    <tr class="total">
-                        <td></td>
-                        
-                        <td>
-                           Total: $385.00
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </body>
-        </html>';
+    // Set email subject
+    $mail->Subject = EMAIL_SUBJECT;
 
-    // Set email body as HTML
-    $mail->MsgHTML($htmlContent);
-    $mail->AltBody = 'This is a plain-text version of the email for non-HTML clients.';
+    // Set the HTML email body based on invoice type or custom email
+    if (empty($custom_email)) {
+        if ($invoice_type == 'invoice') {
+            $mail->MsgHTML(EMAIL_BODY_INVOICE);
+        } elseif ($invoice_type == 'quote') {
+            $mail->MsgHTML(EMAIL_BODY_QUOTE);
+        } elseif ($invoice_type == 'receipt') {
+            $mail->MsgHTML(EMAIL_BODY_RECEIPT);
+        }
+    } else {
+        // Sanitasi custom email tanpa encoding HTML
+        $custom_email = strip_tags($custom_email, '<p><a><br><strong><em>'); // Hapus tag tidak aman
+        $mail->MsgHTML($custom_email); // Kirim email dalam format HTML
+    }
+
+    // Set plain text fallback to empty, since we want only HTML
+    $mail->AltBody = 'ini file idnya '.$fileId; // Kosongkan AltBody
+
+    // Nonaktifkan multi-part MIME dan kirim sebagai HTML saja
     $mail->isHTML(true);
+
+    // Add invoice PDF as an attachment
+	$pdfPath = $_SERVER['DOCUMENT_ROOT'] . "/invoices/" . $fileId . ".pdf";
+    if (file_exists($pdfPath)) {
+		$mail->AddAttachment($pdfPath); // Tambahkan lampiran PDF
+	} else {
+		echo json_encode(array(
+			'status' => 'Error',
+			'message' => 'Invoice file not found.'
+		));
+		exit;
+	}
 
     // Send the email
     if (!$mail->Send()) {
@@ -208,11 +74,10 @@ if ($action == 'email_invoice') {
     } else {
         echo json_encode(array(
             'status' => 'Success',
-            'message' => 'Email has been successfully sent to the customer.'
+            'message' => 'Email has been successfully sent to the customer. '.$_SERVER['DOCUMENT_ROOT']
         ));
     }
 }
-
 
 
 
